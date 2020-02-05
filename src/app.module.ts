874db0f5211130e14ce898b803fc8da36c4682a5
UtopiaBeam <utopiabeam@gmail.com>
2020-02-05 09:40:21 +0700

    feat(config): add config module
    - import `TypegooseModule` for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ConfigService } from './config/config.service';
 
 @Module({
-    imports: [],
+    imports: [
+        TypegooseModule.forRootAsync({
+            useClass: ConfigService,
+        }),
+        ConfigModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
