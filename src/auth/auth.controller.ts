f37417dd7f6dd043120c2c55279dc60f36b7ac5c
UtopiaBeam <utopiabeam@gmail.com>
2020-02-06 16:08:58 +0700

    docs(auth): add tag to auth controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDTO } from './auth.dto';
 import { User } from '../model/user.model';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(private readonly service: AuthService) {}
