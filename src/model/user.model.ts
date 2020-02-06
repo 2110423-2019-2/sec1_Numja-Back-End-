8cf552053d9a8310f14b5fe44c0758e423c5d750
UtopiaBeam <utopiabeam@gmail.com>
2020-02-06 15:45:37 +0700

    docs(user): add user model's fields in swagger

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,5 +1,6 @@
 import { prop, mongoose } from '@typegoose/typegoose';
 import { UserGender, UserRole, UserStatus } from '../enum/user.enum';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class User {
     _id?: mongoose.Types.ObjectId;
@@ -8,33 +9,42 @@ export class User {
         return this._id;
     }
 
+    @ApiProperty()
     @prop({ required: true })
     firstName: string;
 
+    @ApiProperty()
     @prop({ required: true })
     lastName: string;
 
+    @ApiPropertyOptional()
     @prop()
     birthDate?: Date;
 
+    @ApiProperty()
     @prop({ required: true })
     email: string;
 
+    @ApiProperty()
     @prop({ required: true, select: false })
     password: string;
 
+    @ApiPropertyOptional()
     @prop()
     address?: string;
 
+    @ApiProperty({ enum: UserGender })
     @prop({ enum: UserGender, required: true })
     gender: UserGender;
 
+    @ApiProperty()
     @prop({ required: true })
     ssin: string;
 
+    @ApiProperty({ enum: UserRole })
     @prop({ enum: UserRole, required: true })
     role: UserRole;
 
     @prop({ enum: UserStatus, required: true, default: UserStatus.Active })
-    status: UserStatus;
+    status?: UserStatus;
 }
