83a17a70290449570d94eb2601c1ff173275ae64
UtopiaBeam <utopiabeam@gmail.com>
2020-02-06 08:38:53 +0700

    feat(user): add user services
    - add `find()`, `findById(id)` and `findOne(conditions)`

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,4 +8,16 @@ export class UserService {
     constructor(
         @InjectModel(User) private readonly model: ReturnModelType<typeof User>,
     ) {}
+
+    find(): Promise<User[]> {
+        return this.model.find().exec();
+    }
+
+    findById(id: string): Promise<User> {
+        return this.model.findById(id).exec();
+    }
+
+    findOne(conditions: any): Promise<User> {
+        return this.model.findOne(conditions).exec();
+    }
 }
