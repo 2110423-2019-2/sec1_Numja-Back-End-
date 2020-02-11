0c02836a6e8f481a5d3249ececf6679990ca378e
darkotron2 <bee.bee09_@hotmail.co.th>
2020-02-11 19:28:58 +0700

    refractor(admin): remove clutters and unuseful line

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -13,20 +13,6 @@ export class AdminService {
     ) {}
     
     updateStatus(statusDTO: UpdateStatusDTO) : Promise<User> {
-        // const query = { _id: userID };
-        // return this.model.findOneAndUpdate(query, { name: 'jason bourne' }, options, callback)
-
-        // still needed to be validated and checking status to make restrictedd and constrain d....
-        // let updatedUser : User;
-        // this.model.findById(statusDTO.id, function (err, user) {
-        //     if (err) {
-        //         console.log(err)
-        //     }
-        //     user.status = statusDTO.status
-        //     user.save();
-        //     updatedUser = user
-        // })
-        // return updatedUser
         let options = {new: true}
         return this.model.findByIdAndUpdate(statusDTO.id, {status : statusDTO.status},options).exec()
     }
