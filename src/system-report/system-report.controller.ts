b68a43acd94585160f1928199994467e55073c30
Tanawit Kritwongwimian <tanawit139@gmail.com>
2020-02-12 23:23:23 +0700

    feat(report) add auth guard
    - add auth guard to user report endpoint
    - add auth guard to system report endpoint

--- src/system-report/system-report.controller.ts
+++ src/system-report/system-report.controller.ts
@@ -1,22 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { SystemReportService } from './system-report.service';
 import { SystemReport } from '../model/system-report.model';
+import { AuthGuard } from '../guards/auth.guard';
 
 @ApiBearerAuth()
 @ApiTags('Report')
 @Controller('report/system')
 export class SystemReportController {
     constructor(private readonly service: SystemReportService) {}
-
+    @UseGuards(AuthGuard)
     @Get()
     all() {
         return this.service.find();
     }
+    @UseGuards(AuthGuard)
     @Get(':id')
     one(@Param('id') reportId: string) {
         return this.service.findById(reportId);
     }
+    @UseGuards(AuthGuard)
     @Post()
     create(@Body() report: SystemReport) {
         return this.service.create(report);
--- src/user-report/user-report.controller.ts
+++ src/user-report/user-report.controller.ts
@@ -1,22 +1,25 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UserReportService } from './user-report.service';
 import { UserReport } from '../model/user-report.model';
+import { AuthGuard } from '../guards/auth.guard';
 
 @ApiBearerAuth()
 @ApiTags('Report')
 @Controller('report/user')
 export class UserReportController {
     constructor(private readonly service: UserReportService) {}
-
+    @UseGuards(AuthGuard)
     @Get()
     all() {
         return this.service.find();
     }
+    @UseGuards(AuthGuard)
     @Get(':id')
     one(@Param('id') reportId: string) {
         return this.service.findById(reportId);
     }
+    @UseGuards(AuthGuard)
     @Post()
     create(@Body() report: UserReport) {
         return this.service.create(report);
