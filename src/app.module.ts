8913ea4212e48cc084a25b15944bd7d230bade48
Tanawit Kritwongwimian <tanawit139@gmail.com>
2020-02-14 18:01:44 +0700

    opt(app module): update import statement to match upstream

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
