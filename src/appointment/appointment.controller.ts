19ca35fa030e1b14b02f616d34d1df90241b7d95
DarkXenoZ <darkxenoz@gmail.com>
2020-02-16 10:00:38 +0700

    feat(appointment): add and create appointment controller

--- src/appointment/appointment.controller.ts
+++ src/appointment/appointment.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch } from '@nestjs/common';
+import { AppointmentService } from './appointment.service';
+import { UserId } from 'src/decorators/user-id.decorator';
+import { AppointmentDTO } from './appointment.dto';
 
 @Controller('appointment')
-export class AppointmentController {}
+export class AppointmentController {
+    constructor(private readonly service: AppointmentService) {}
+
+    @Post()
+    createAppointment(
+        @Body() appointment: AppointmentDTO,
+        @UserId() studentId: string,
+    ) {
+        return this.service.createAppointment(appointment, studentId);
+    }
+
+    @Get()
+    getAllAppointments() {
+        return this.service.getAllAppointments();
+    }
+
+    @Get('id/:id')
+    findById(@Param('id') id: string) {
+        return this.service.findById(id);
+    }
+
+    @Get()
+    findByUserId(@UserId() userId: string) {
+        return this.service.findByUserId(userId);
+    }
+
+    @Patch()
+    cancelAppointment() {}
+
+    @Patch()
+    acceptAppointment() {}
+
+    @Patch()
+    finishAppointment() {}
+}
