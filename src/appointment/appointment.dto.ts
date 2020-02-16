c2323b42fbaa73d391808b8048451d86e0d7f129
DarkXenoZ <darkxenoz@gmail.com>
2020-02-16 16:40:30 +0700

    refractor(appointment): remove prop and use extends

--- src/appointment/appointment.dto.ts
+++ src/appointment/appointment.dto.ts
@@ -1,42 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { prop } from '@typegoose/typegoose';
 
-export class AppointmentDTO {
+export class EditAppointmentDTO {
     @ApiProperty()
-    @prop({ required: true })
     startTime: Date;
 
     @ApiProperty()
-    @prop({ required: true })
     endTime: Date;
 
     @ApiProperty()
-    @prop({ required: true })
     location: string;
 
     @ApiProperty()
-    @prop({ required: true })
     price: number;
-
-    @ApiProperty()
-    @prop({ required: true})
-    tutorId: string;
 }
 
-export class EditAppointmentDTO{
+export class CreateAppointmentDTO extends EditAppointmentDTO {
     @ApiProperty()
-    @prop({ required: true })
-    startTime: Date;
-
-    @ApiProperty()
-    @prop({ required: true })
-    endTime: Date;
-
-    @ApiProperty()
-    @prop({ required: true })
-    location: string;
-
-    @ApiProperty()
-    @prop({ required: true })
-    price: number;
+    tutorId: string;
 }
