13c558c95208c639974adf171d12055134994772
DarkXenoZ <darkxenoz@gmail.com>
2020-02-16 12:17:17 +0700

    feat(appointment): create new DTO for edit appointment

--- src/appointment/appointment.dto.ts
+++ src/appointment/appointment.dto.ts
@@ -19,5 +19,24 @@ export class AppointmentDTO {
     price: number;
 
     @ApiProperty()
+    @prop({ required: true})
     tutorId: string;
+}
+
+export class EditAppointmentDTO{
+    @ApiProperty()
+    @prop({ required: true })
+    startTime: Date;
+
+    @ApiProperty()
+    @prop({ required: true })
+    endTime: Date;
+
+    @ApiProperty()
+    @prop({ required: true })
+    location: string;
+
+    @ApiProperty()
+    @prop({ required: true })
+    price: number;
 }
