12398c446cc165b0087480461219a02f26a7022d
DarkXenoZ <darkxenoz@gmail.com>
2020-02-16 16:40:54 +0700

    refractor(appointment): remove ApiProperty

--- src/model/appointment.model.ts
+++ src/model/appointment.model.ts
@@ -1,5 +1,4 @@
 import { prop, mongoose, Ref } from '@typegoose/typegoose';
-import { ApiProperty } from '@nestjs/swagger';
 import { User } from './user.model';
 import { AppointmentStatus } from 'src/enum/appointment.enum';
 
@@ -10,31 +9,24 @@ export class Appointment {
         return this._id;
     }
 
-    @ApiProperty()
     @prop({ required: true })
     startTime: Date;
 
-    @ApiProperty()
     @prop({ required: true })
     endTime: Date;
 
-    @ApiProperty()
     @prop({ required: true })
     location: string;
 
-    @ApiProperty()
     @prop({ required: true })
     price: number;
 
-    @ApiProperty()
     @prop({ ref: User })
     student: Ref<User>;
 
-    @ApiProperty()
     @prop({ ref: User })
     tutor: Ref<User>;
 
-    @ApiProperty()
     @prop({ enum: AppointmentStatus, default: AppointmentStatus.Pending })
     status: AppointmentStatus;
 }
