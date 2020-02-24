fd111e6a1c4ed015a76de60f14497c3eaaf59401
Tanawit Kritwongwimian <tanawit139@gmail.com>
2020-02-24 21:12:53 +0700

    refactor(admin): remove unused import

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from '../admin/admin.controller';
-import { UserModule } from '../user/user.module';
 import { AdminService } from '../admin/admin.service';
 
 @Module({
