2abead02d7103a53caea2b53543ef868e18c4a9d
DarkXenoZ <darkxenoz@gmail.com>
2020-02-26 16:20:29 +0700

    type(transaction): create DTO for transaction controller

--- src/transaction/transaction.dto.ts
+++ src/transaction/transaction.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { TransactionType } from '../enum/transaction.enum';
+import { Base } from '@typegoose/typegoose/lib/defaultClasses';
 
 export class TransactionDTO {
     type: TransactionType;
@@ -7,6 +8,19 @@ export class TransactionDTO {
     senderId?: string;
     receiverId?: string;
     amount: number;
+}
+
+export class BaseTransactionDTO {
+    @ApiProperty({ required: true })
+    amount: number;
+}
+
+export class TransferTransactionDTO extends BaseTransactionDTO {
+    @ApiProperty({ required: true })
+    senderId: string;
+
+    @ApiProperty({ required: true })
+    receiverId: string;
 }
 
 export class AdminTransferTransactionDTO {
