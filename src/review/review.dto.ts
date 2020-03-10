1c914be4bdadf82e14c287328087522a498c3dad
DarkXenoZ <darkxenoz@gmail.com>
2020-03-10 20:51:08 +0700

    fix(review): change DTO to update and review DTO seperately

--- src/review/review.dto.ts
+++ src/review/review.dto.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class ReviewDTO {
+export class UpdateReviewDTO{
+
     @ApiProperty({ required: true })
     comment: string;
 
     @ApiProperty({ required: true })
     rating: number;
+}
 
+export class CreateReviewDTO extends UpdateReviewDTO{
+    
     @ApiProperty({ required: true })
     reviewedUserId: string;
 }
