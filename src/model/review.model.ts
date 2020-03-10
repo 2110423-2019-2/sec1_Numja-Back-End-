f4e230bf24c632c689693c43e601b3a42c1eccca
DarkXenoZ <darkxenoz@gmail.com>
2020-03-10 19:54:11 +0700

    fix(review): remove title and description, add comment and rating

--- src/model/review.model.ts
+++ src/model/review.model.ts
@@ -5,14 +5,14 @@ export class Review {
     _id?: mongoose.Types.ObjectId;
 
     @prop({ required: true })
-    title: string;
-
-    @prop({ required: true })
-    description: string;
+    comment: string;
 
     @prop({ ref: User, required: true })
     reviewerUser: Ref<User>;
 
     @prop({ ref: User })
     reviewedUser: Ref<User>;
+
+    @prop({ required: true, min: 1, max: 5})
+    rating: number;
 }
